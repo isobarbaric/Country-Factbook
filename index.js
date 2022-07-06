b8f94124c2e8143c648c0c305ce10b6db3aae717
Krish Shah <shahkrish2016@gmail.com>
2022-07-05 20:57:30 -0400

    clickable functionality

--- index.js
+++ index.js
@@ -32,56 +32,136 @@ class Country {
 
 // have to deal with 404, 500 errors
 const userAction = async (userInput) => {
-    let response = await fetch('https://restcountries.com/v3.1/name/' + userInput)
+    let response = null;
+
+    try {
+        response = await fetch('https://restcountries.com/v3.1/name/' + userInput)
+    } catch(error) {
+        return "Invalid";
+    }
+
     let jsonified = await response.json()
 
     // response checking not working
     // console.log(response.status);
 
-    if (response.status == 404 || response.status == 500) {
-        return "Invalid"
+    return jsonified
+}
+
+const getCountryData = async (countryAbbr) => {
+    let response = null;
+
+    try {
+        response = await fetch('https://restcountries.com/v3.1/alpha/' + countryAbbr)
+    } catch(error) {
+        return "Invalid";
     }
+
+    let jsonified = await response.json()
+
+    // response checking not working
+    // console.log(response.status);
+
     return jsonified
 }
 
 function init() {
     let textBox = document.getElementById('text-box');    
     textBox.addEventListener('input', function() {
+        // clear contents of right div
+        let rightDiv = document.getElementById('not-footer');
+        rightDiv.innerHTML = "<h3 id='placeholder'> Enter a country to get started!</h3>";
 
         // prevents empty string from being pushed to the api
         if (textBox.value.length == 0) {
+            let table = document.getElementById('search-results');
+            table.innerHTML = "";
             return 
         }
 
         let json_response = userAction(textBox.value);
 
         Promise.allSettled([json_response]).then(function(countries) {
-            if (json_response == "Invalid") {
-                return
-            }
         
+            // cloning the list of relevant countries and sorting them by population
             let clonedCountries = Object.assign([], countries[0]['value']);
-
-            // sorting doesn't show up properly on Chrome for some reason (works on Safari)
-            clonedCountries.sort((a, b) => parseInt(a['population']) < parseInt(b['population']));
-            console.log(clonedCountries);
+            clonedCountries.sort((a, b) => parseInt(b['population']) - parseInt(a['population']));
 
             // delete existing rows
             let table = document.getElementById('search-results');
             table.innerHTML = '';
 
-            // add new rows
-            for (let i = 0; i < clonedCountries.length; i++) {
-                let tr = "<tbody class='country'>";
-                tr += "<tr><td rowspan='2'><img src='" + clonedCountries[i]['flags']['png'] + "' alt='flag'></td><td class='center'>" + clonedCountries[i]['cca3'] + "</td></tr><tr><td class='center'>" + clonedCountries[i]['name']['official'] + "</td></tr>"
-                tr += '</tbody>';
-                console.log(tr);
-                table.innerHTML += tr;
+            try {
+                // add new rows
+                for (let i = 0; i < clonedCountries.length; i++) {
+                    id_name = clonedCountries[i]['cca3']
+
+                    let tr = "<tbody class='country' id='" + id_name + "'>";
+                    tr += "<tr onclick='serveContent(" + id_name + ")'><td rowspan='2'><img src='" + clonedCountries[i]['flags']['png'] + "' alt='flag' class='flag-left'></td><td class='center'><b>" + clonedCountries[i]['cca3'] + "</b></td></tr><tr onclick='serveContent(" + id_name + ")'><td class='center'>" + clonedCountries[i]['name']['official'] + "</td></tr>"
+                    tr += '</tbody>';
+                  
+                    table.innerHTML += tr;
+                }
+            } catch(error) {
+                table.innerHTML = "<h3 id='not-found'> No Countries Found! </h3>";
             }
 
         });
     });
 }
+
+function serveContent(country_name) {
+    country_abbr = country_name.id;
+    let json_response = getCountryData(country_abbr);
+
+    Promise.allSettled([json_response]).then(function(countries) {
+        let clonedCountry = Object.assign([], countries[0]['value'])[0];
+        
+        let rightDiv = document.getElementById('not-footer');
+        
+        //  flag
+        rightDiv.innerHTML = "<img src=" + clonedCountry['flags']['png'] + " alt='" + clonedCountry['name']['common']  + "' id='flag-right'>";
+      
+        // name 
+        rightDiv.innerHTML += "<b>Name: </b> " + clonedCountry['name']['official'];
+        
+        // 
+
+    });
+}
+
+function clearContent() {
+}
+
+// function singleWordify(name) {
+//     const words = name.split(' ');
+//     if (words.length == 1) {
+//         return words[0];
+//     }
+//     let simplified = '';
+//     for (let i = 0; i < words.length; i++) {
+//         simplified += words[i];
+//         if (i != words.length-1) {
+//             simplified += '_';
+//         }
+//     }
+//     return simplified;
+// }
+
+// function complexify(name) {
+//     const words = name.split('_');
+//     if (words.length == 1) {
+//         return words[0];
+//     }
+//     let simplified = '';
+//     for (let i = 0; i < words.length; i++) {
+//         simplified += words[i];
+//         if (i != words.length-1) {
+//             simplified += ' ';
+//         }
+//     }
+//     return simplified;
+// }
 
 document.addEventListener('readystatechange', function() {
     if (document.readyState === "complete") {
